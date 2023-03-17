82ff576831cdfea39edd066e0b3bf9872e640247
holyocheese <543296676@qq.com>
2023-03-17 17:04:48 +0800

    save

--- src/main/java/com/api/gateway/ServiceZuulApplication.java
+++ src/main/java/com/api/gateway/ServiceZuulApplication.java
@@ -2,23 +2,12 @@ package com.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import com.tuji.filter.UserLoginFilter;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableDiscoveryClient
 public class ServiceZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceZuulApplication.class, args);
 	}
 	
-	@Bean
-	public UserLoginFilter userLoginFilter() {
-		return new UserLoginFilter();
-	}
 }
